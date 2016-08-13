9e95e94f2a6fec3d9d73b89c7ade0cb8725965c3
Lyubaev Kirill <lubaevka@gmail.com>
2016-08-13 13:18:39 +0300

    Fix migrate.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,8 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('email')->unique();
-            $table->string('password');
             $table->string('username');
+            $table->string('password');
             $table->string('name')->nullable();
             $table->rememberToken();
             $table->timestamps();
