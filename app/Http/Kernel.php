3370b9668f28788a44793b29880c3a63c2c46240
Lyubaev Kirill <lubaevka@gmail.com>
2016-08-13 13:51:33 +0300

    Fix route middleware.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -54,7 +54,7 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
 
-        'jwt.auth' => Tymon\JWTAuth\Middleware\GetUserFromToken::class,
-        'jwt.refresh' => Tymon\JWTAuth\Middleware\RefreshToken::class,
+        'jwt.auth' => \Tymon\JWTAuth\Middleware\GetUserFromToken::class,
+        'jwt.refresh' => \Tymon\JWTAuth\Middleware\RefreshToken::class,
     ];
 }
