7ee2fad81a0706e3b09333877b16dd46bda317fe
Lyubaev Kirill <lubaevka@gmail.com>
2016-08-13 13:19:14 +0300

    Rename Auth request files.

--- app/Http/Requests/AuthLoginRequest.php
+++ app/Http/Requests/AuthLoginRequest.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Requests;
 
-use App\Http\Requests\Request;
-
-class SignInRequest extends Request
+class AuthLoginRequest extends Request
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -23,6 +21,6 @@ class SignInRequest extends Request
      */
     public function rules()
     {
-        return ['email' => 'required|email|max:255'];
+        return ['username' => 'required', 'password' => 'required'];
     }
 }
