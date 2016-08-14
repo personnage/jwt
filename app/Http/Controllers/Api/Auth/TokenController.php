bc2e18c9c171cb37c731592b37444254b833427b
Lyubaev Kirill <lubaevka@gmail.com>
2016-08-14 15:16:54 +0300

    New TokenController.

---
+++ app/Http/Controllers/Api/Auth/TokenController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+use App\Http\Controllers\Api\Controller;
+use Illuminate\Http\Request;
+
+class TokenController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('jwt.auth', ['only' => ['payload']]);
+        $this->middleware('jwt.refresh', ['only' => ['forceRefresh']]);
+    }
+
+    public function payload()
+    {
+        $payload = JWTAuth::parseToken()->getPayload()->toArray();
+
+        return $this->respond(compact('payload'));
+    }
+
+    public function refresh(Request $request)
+    {
+        try {
+           $token = JWTAuth::parseToken()->refresh();
+        } catch (TokenInvalidException $e) {
+            // 400
+            return $this->setStatusCode($e->getStatusCode())
+                ->respondWithError('Could not decode token')
+            ;
+        } catch (TokenExpiredException $e) {
+            // 401
+            return $this->setStatusCode($e->getStatusCode())
+                ->respondWithError('Token has expired and can no longer be refreshed')
+            ;
+        } catch (JWTException $e) {
+            // 500
+            return $this->setStatusCode($e->getStatusCode())
+                ->respondWithError('The token could not be parsed from the request')
+            ;
+        }
+
+        return $this->respond(compact('token'));
+    }
+
+    public function forceRefresh()
+    {
+        return $this->respondEmpty();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,12 @@ Route::group(['prefix' => 'v1', 'namespace' => 'Api'], function () {
     Route::post('login', 'Auth\AuthController@login');
     Route::post('register', 'Auth\AuthController@register');
 
+    Route::group(['prefix' => 'token', 'namespace' => 'Auth'], function () {
+        Route::get('payload', 'TokenController@payload');
+        Route::get('refresh', 'TokenController@refresh');
+        Route::post('refresh', 'TokenController@forceRefresh');
+    });
+
     // Restricted
     Route::group(['middleware' => 'jwt.auth'], function () {
         Route::get('home', 'HomeController@index');
